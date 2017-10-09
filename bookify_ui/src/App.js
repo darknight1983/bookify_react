879f72219f65498fa678772c7a9d7ff531c29ab0
Jacolby Green <darknightnov@gmail.com>
2017-10-08 22:05:42 -0500

    Added fetch api to request json from google books api

--- bookify_ui/src/App.js
+++ bookify_ui/src/App.js
@@ -1,21 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-class App extends Component {
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      books: []
+    }
+  }
+
+  handleClick(e) {
+    if (e.keyCode === 13) {
+      let query = e.target.value;
+      fetch("https://www.googleapis.com/books/v1/volumes?q=" + query)
+        .then((response) => {
+          var contentType = response.headers.get("content-type");
+          if (contentType && contentType.includes("application/json")) {
+            return response.json()
+          }
+          throw new TypeError("Oops, we havent got JSON!");
+        }).then((booksArray) => {
+          console.log(booksArray.items);
+          console.log(this)
+          this.setState({books: booksArray});
+        }).catch((error) => {
+          console.error(error);
+        })
+      e.target.value = ""
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <header>
+        <h1>Bookify</h1>
+        <input type="text" onKeyUp={this.handleClick.bind(this)} />
+      </header>
+
+    )
   }
 }
 
-export default App;
+export default App
--- bookify_ui/src/index.js
+++ bookify_ui/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './app';
+// var GOOGLE_API_KEY = AIzaSyDGbeWcI9o5f2jmPLff4wNkUgGdYHhVD_M
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
