a668391cd31d7e9aa80fc7d7899600c0e4059739
Jacolby Green <darknightnov@gmail.com>
2017-10-20 14:50:32 -0500

    Added styling for header and navigation.

--- bookify_ui/src/App.js
+++ bookify_ui/src/App.js
@@ -33,22 +33,25 @@ class App extends React.Component {
     return (
       <div>
         <header className="header">
-          <h1 className="siteName">Bookify</h1>
-          {/* Binding handleClick to instances of App. */}
-          <input className="author_query" type="text" onKeyUp={this.handleClick.bind(this)} />
-          <nav>
-            <ul>
-              <li><a href="/auth/logout">Logout</a></li>
-              <li><a href="/auth/login">Login</a></li>
-              <li><a href="/home"></a>Home</li>
-            </ul>
-          </nav>
+          <div className="header__inner">
+            <img className="header__logo" src="../images/city.png" alt="iconic view of a cityscape" />
+            <h1 className="header__title">Bookify</h1>
+            {/* Binding handleClick to instances of App. */}
+            <input className="author_query" type="text" onKeyUp={this.handleClick.bind(this)} />
+          </div>
         </header>
-        <section>
+        <nav className="nav">
+          <ul className="nav__list">
+            <li className="nav__item"><a href="/auth/logout">Logout</a></li>
+            <li className="nav__item"><a href="/auth/login">Login</a></li>
+            <li className="nav__item" ><a href="/home"></a>Home</li>
+          </ul>
+        </nav>
+        <main>
           {this.state.books.map(function(book, i) {
             return <Book title={book.volumeInfo.title} key={i}/>
           })}
-        </section>
+        </main>
       </div>
     )
   }
